3c935418bed045cfaf4417b398bce62582b599d8
kecleon <ultimatedood@gmail.com>
2025-06-15 03:07:16 -0700

    empty mod template

---
+++ ManyLives/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.NET.Common;
+using HarmonyLib;
+
+namespace ManyLives;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+	internal static new ManualLogSource Log;
+	
+	public static int MaxLives = 1;
+	public static int Lives = 1;
+
+	public override void Load()
+	{
+		// Plugin startup logic
+		Log = base.Log;
+		Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+	}
+}
+
+[HarmonyPatch(typeof(PlayerAvatar), "Awake")]
+public static class PlayerAvatar_Awake_Patch
+{
+	private static void Postfix(PlayerAvatar __instance)
+	{
+		
+	}
+}
+
+[HarmonyPatch(typeof(RunManager), "ChangeLevel")]
+public static class RunManager_ChangeLevel_Patch
+{
+	private static void Postfix(RunManager __instance)
+	{
+		Plugin.Lives = Plugin.MaxLives;
+	}
+}
